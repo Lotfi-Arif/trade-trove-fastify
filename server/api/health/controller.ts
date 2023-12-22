57bf0e136e21ab4289744a47c8a01904f3a47d94
Lotfi Arif <lotfi@ninetailed.com>
2023-12-23 01:49:54 +0700

    Refactor health controller to include task, order, product, and user counts

--- server/api/health/controller.ts
+++ server/api/health/controller.ts
@@ -6,7 +6,19 @@ export default defineController(() => ({
     status: 200,
     body: {
       server: 'ok',
-      db: await prismaClient.task
+      task: await prismaClient.task
+        .count()
+        .then(() => 'ok' as const)
+        .catch(() => 'ng' as const),
+      order: await prismaClient.order
+        .count()
+        .then(() => 'ok' as const)
+        .catch(() => 'ng' as const),
+      product: await prismaClient.product
+        .count()
+        .then(() => 'ok' as const)
+        .catch(() => 'ng' as const),
+      user: await prismaClient.user
         .count()
         .then(() => 'ok' as const)
         .catch(() => 'ng' as const),
